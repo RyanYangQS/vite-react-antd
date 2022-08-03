d89c8c084ccc50d1b168a45f7ea0c81f7e29b315
jiqishoubi <437753880@qq.com>
2022-08-03 14:51:49 +0800

    feat: 路由懒加载

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,7 +1,12 @@
+import { lazy, Suspense } from 'react'
 import { Navigate } from 'react-router-dom'
 import LoginLayout from '@/layouts/LoginLayout'
-import Login from '@/pages/login'
-import Home from '@/pages/home'
+import LazyLoading from '@/components/LazyLoading'
+
+const route404 = {
+  path: '*',
+  element: () => import('@/pages/common/404'),
+}
 
 const routes = [
   {
@@ -11,15 +16,43 @@ const routes = [
       { path: '', element: <Navigate to="login" /> }, // Redirect
       {
         path: 'login',
-        element: <Login />,
+        element: () => import('@/pages/login'),
       },
       {
         path: 'home',
-        element: <Home />,
+        element: () => import('@/pages/home'),
       },
-      // route404,
+      route404,
     ],
   },
 ]
+
+// 懒加载
+
+function LazyElement(props) {
+  const { importFunc } = props
+  const LazyComponent = lazy(importFunc)
+  return (
+    <Suspense fallback={<LazyLoading />}>
+      <LazyComponent />
+    </Suspense>
+  )
+}
+
+// 处理routes 如果element是懒加载，要包裹Suspense
+function dealRoutes(routesArr) {
+  if (routesArr && Array.isArray(routesArr) && routesArr.length > 0) {
+    routesArr.forEach((route) => {
+      if (route.element && typeof route.element == 'function') {
+        const importFunc = route.element
+        route.element = <LazyElement importFunc={importFunc} />
+      }
+      if (route.children) {
+        dealRoutes(route.children)
+      }
+    })
+  }
+}
+dealRoutes(routes)
 
 export default routes
