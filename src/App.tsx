1c076bea8a1a5ab7742a84b1711c49d62c50b1f6
jiqishoubi <437753880@qq.com>
2022-08-03 14:16:41 +0800

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import './App.css'
+import './App.less'
 import React from 'react'
 import { BrowserRouter, useRoutes } from 'react-router-dom'
 import routes from '@/routes'
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,14 @@
 import React from 'react'
+import { Button } from 'antd'
 
 const Index: React.FC = () => {
-  return <>home</>
+  return (
+    <>
+      home
+      <div>
+        <Button>按钮</Button>
+      </div>
+    </>
+  )
 }
 export default Index
